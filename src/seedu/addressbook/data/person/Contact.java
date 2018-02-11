1656623620d7c4ad8622854bff8da5aaf3d24a5d
Jethro Kuan <jethrokuan95@gmail.com>
2018-02-11 18:21:27 +0800

    Extract Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+    public String value;
+    private boolean isPrivate;
+
+    public Contact(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+
+}
